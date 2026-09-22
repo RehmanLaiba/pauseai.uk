6af3781c0cd6cab3d35478de8fdb8b8643cd3963
RehmanLaiba <rehmanlaiba@proton.me>
2026-09-22 13:32:24 +0100

    swapped wrong image i think oops, should be good now

--- app/track-record/page.tsx
+++ app/track-record/page.tsx
@@ -778,14 +778,14 @@ export default function TrackRecordPage() {
                   <div className="gallery gallery-4">
                     <a
                       className="shot shot-tall"
-                      href="/images/downing-street-protest-sept-2026/downing-street-gates-crowd.jpg"
+                      href="/images/downing-street-protest-sept-2026/downing-street-global-treaty.jpg"
                     >
                       <Image
                         fill
                         sizes="(max-width: 768px) 100vw, 25vw"
                         style={{ objectFit: "cover" }}
-                        src="/images/downing-street-protest-sept-2026/downing-street-gates-crowd-thumb.jpg"
-                        alt="Protesters with PauseAI banners and placards outside the gates of Downing Street"
+                        src="/images/downing-street-protest-sept-2026/downing-street-global-treaty-thumb.jpg"
+                        alt="Two volunteers holding a Global Treaty PauseAI sign above a large pause symbol"
                       />
                     </a>
                     <a
